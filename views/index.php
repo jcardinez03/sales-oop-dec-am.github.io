0c644fea9ac0add7173c6646d517063db4f7c3a2
Marwin Jade <jcardinez03@gmail.com>
2024-12-09 09:40:52 +0800

    first upload

---
+++ views/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Login</title>
+</head>
+<body class="bg-light">
+    <div style="height: 100vh">
+    <div class="row h-100 m-0">
+            <div class="card w-25 my-auto mx-auto">
+                <div class="card-header bg-white border-0 py-3">
+                    <h1 class="text-center">LOGIN</h1>
+                </div>
+                <div class="card-body">
+                    <form action="../actions/login.php" method="post">
+                        <div class="mb-3">
+                            <input type="text" name="username" id="username" placeholder="USERNAME" class="form-control" required autofocus>
+                        </div>
+
+                        <div class="mb-3">
+                            <input type="password" name="password" id="password" placeholder="PASSWORD" class="form-control">
+                        </div>
+
+                        <button type="submit" class="btn btn-primary w-100">Log in</button>
+                    </form>
+
+                    <p class="text-center mt-3 small"><a href="../views/register.php">Create Account</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+
+    <!-- JS BOOTSTRAP -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> 
+</body>
+</html>
